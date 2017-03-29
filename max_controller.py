77ecb3f8510f1f037b38bf01415aaddbf904432c
Prudhvi Boyapalli <prrb02@gmail.com>
2017-03-28 22:21:51 -0500

    Added pages for each user; Added many more interaction elements; Added more sophisticated data updates and queries

--- max_controller.py
+++ max_controller.py
@@ -1,8 +1,10 @@
 import json
+import random
 from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
-value = 10
+
+### Page Rendering
 
 @app.route('/')
 def index():
@@ -11,22 +13,107 @@ def index():
     """
     return render_template('controller.html')
 
+@app.route('/j')
+def j():
+    return render_template('j.html')
 
-@app.route('/value')
-def get_value():
-    global value
-    return str(value)
+@app.route('/p')
+def p():
+    return render_template('p.html')
 
-@app.route('/increment')
-def increment():
-    global value
-    value += 1
-    print value
+@app.route('/s')
+def s():
+    return render_template('s.html')
+
+
+### Data Updates
+
+@app.route('/slider1/<user>')
+def slider1(user):
+    global user_data
+
+    user_data[user]["slider1"] = request.args["value"]
+
     return ""
 
-@app.route('/decrement')
-def decrement():
-    global value
-    value -= 1
-    print value
+@app.route('/slider2/<user>')
+def slider2(user):
+    global user_data
+
+    user_data[user]["slider2"] = request.args["value"]
+
     return ""
+
+@app.route('/slider3/<user>')
+def slider3(user):
+    global user_data
+
+    user_data[user]["slider3"] = request.args["value"]
+
+    return ""
+
+@app.route('/increment/<user>')
+def increment(user):
+    global user_data
+    user_data[user]["value"] += 1
+    return ""
+
+@app.route('/decrement/<user>')
+def decrement(user):
+    global user_data
+    user_data[user]["value"] -= 1
+    return ""
+
+@app.route('/random_float/<user>')
+def random_float(user):
+    global user_data
+    user_data[user]["random_float"] = random.random()
+    return ""
+
+@app.route('/random_int/<user>')
+def random_int(user):
+    global user_data
+    user_data[user]["random_int"] = random.randint(0, 99)
+    return ""
+
+
+### Get User Data
+
+@app.route('/get/<user>')
+def get_all_values(user):
+    global user_data
+    return json.dumps(user_data[user])
+
+@app.route('/get/<user>/<kind>')
+def get_value(user, kind):
+    global user_data
+    return str(user_data[user][kind])
+
+
+### Reset User Data
+
+@app.route('/reset/<user>')
+def reset(user):
+    global user_data
+
+    user_data[user] = {
+            "slider1" : 0,
+            "slider2" : 0,
+            "slider3" : 0,
+            "value" : 0,
+            "random_float": 0.0,
+            "random_int" : 0
+    }
+
+    return "Reset data for user: '" + user + "'"
+
+
+
+### Initialization
+
+user_data = {}
+reset("j")
+reset("p")
+reset("s")
+
+
--- static/scripts/controller.js
+++ static/scripts/controller.js
@@ -1,8 +1,40 @@
 
-function increment() {
-    $.get("/increment")
+function slider1(user) {
+    var value = $("#slider1")[0].value;
+    $.get("/slider1/" + user, { "value" : value });
 }
 
-function decrement() {
-    $.get("/decrement")
+function slider2(user) {
+    var value = $("#slider2")[0].value;
+    $.get("/slider2/" + user, { "value" : value });
+}
+
+function slider3(user) {
+    var value = $("#slider3")[0].value;
+    $.get("/slider3/" + user, { "value" : value });
+}
+
+function increment(user) {
+    $.get("/increment/" + user);
+}
+
+function decrement(user) {
+    $.get("/decrement/" + user);
+}
+
+function randomFloat(user) {
+    $.get("/random_float/" + user);
+}
+
+function randomInt(user) {
+    $.get("/random_int/" + user);
+}
+
+function reset_all_data() {
+    if (confirm("Are you sure?")) {
+        $.get("/reset/j");
+        $.get("/reset/p");
+        $.get("/reset/s");
+        alert("Cleared all data.")
+    }
 }
